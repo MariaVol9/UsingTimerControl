c574c82b009bcabff3f7f180b6c4270cab220b15
margolevi977@gmail.com <margolevi977@gmail.com>
2020-07-18 23:00:51 +0300

    Add project files.

---
+++ UsingTimerControl/Form1.Designer.cs
@@ -0,0 +1,58 @@
+﻿namespace UsingTimerControl
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.timerTick1 = new TimerTickControl.TimerTick();
+            this.SuspendLayout();
+            // 
+            // timerTick1
+            // 
+            this.timerTick1.Location = new System.Drawing.Point(0, 0);
+            this.timerTick1.Name = "timerTick1";
+            this.timerTick1.Size = new System.Drawing.Size(800, 450);
+            this.timerTick1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.timerTick1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private TimerTickControl.TimerTick timerTick1;
+    }
+}
+
